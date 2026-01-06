84aa5059a5c440520fb86ee081cbc0e4e811d89d
gandalfg <tasos.valtinos@gmail.com>
2026-01-06 17:52:32 +0100

    Added provider, configuration should be ready

---
+++ frontend/components/Providers.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import React from "react";
+import {
+  AuthProvider,
+  OpenfortProvider,
+  getDefaultConfig,
+  RecoveryMethod,
+} from "@openfort/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider, createConfig } from "wagmi";
+import { baseSepolia, sepolia } from "viem/chains";
+
+const config = createConfig(
+  getDefaultConfig({
+    appName: "Openfort Demo App",
+    chains: [baseSepolia, sepolia], // add all the chains you want to support
+    ssr: true,
+  })
+);
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <OpenfortProvider
+          // Set the publishable key of your Openfort account. This field is required.
+          publishableKey={process.env.NEXT_PUBLIC_OPENFORT_PUBLISHABLE_KEY || "YOUR_OPENFORT_PUBLISHABLE_KEY"}
+
+          // Set the wallet configuration.
+          walletConfig={{
+            shieldPublishableKey: process.env.NEXT_PUBLIC_OPENFORT_SHIELD_PUBLISHABLE_KEY || "YOUR_OPENFORT_SHIELD_PUBLISHABLE_KEY",
+            // If you want to use AUTOMATIC embedded wallet recovery, an encryption session is required.
+            // Set this to your recovery endpoint URL from step "3. Set up the recovery endpoint" (e.g., "https://your-domain.com/api/protected-create-encryption-session").
+            createEncryptedSessionEndpoint: process.env.NEXT_PUBLIC_OPENFORT_BACKEND_ENDPOINT || "YOUR_OPENFORT_BACKEND_ENDPOINT",
+          }}
+
+          uiConfig={{
+            authProviders: [
+              AuthProvider.EMAIL,
+              AuthProvider.GUEST,
+              AuthProvider.GOOGLE,
+              AuthProvider.WALLET,
+              // Add your own authentication providers here...
+              // More information in https://www.openfort.io/docs/products/embedded-wallet/react/auth
+            ],
+            walletRecovery: {
+              defaultMethod: RecoveryMethod.AUTOMATIC, // or RecoveryMethod.PASSKEY or RecoveryMethod.PASSWORD
+            },
+          }}
+        >
+          {children}
+        </OpenfortProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
